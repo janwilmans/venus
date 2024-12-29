8bcdc2f4c57f2a5bd9cf2b6a9d5a7e9d923d82a9
Jan Wilmans <janwilmans@gmail.com>
2024-12-29 16:09:04 +0100

    Added configuration files and api documentation

--- src/executor/include/executor/guarded.hpp
+++ src/executor/include/executor/guarded.hpp
@@ -32,6 +32,9 @@ private:
     std::condition_variable m_condition;
 
 public:
+    /**
+     * @brief executes @p action after waiting for @p condition, where @p action returns a result
+     */
     template <typename Action>
     auto with_lock(Action && action)
     {
@@ -39,13 +42,20 @@ public:
         return action(m_data);
     }
 
+    /**
+     * @brief wait for the @p condition to be true
+     */
     template <typename Condition>
-    auto wait_for(Condition && condition)
+    void wait_for(Condition && condition)
     {
         std::unique_lock<std::mutex> lock(m_mutex);
-        return m_condition.wait(lock, [&]() { return condition(m_data); });
+        m_condition.wait(lock, [&]() { return condition(m_data); });
     }
 
+    /**
+     * @brief wait for the @p condition to be true or a timeout occurs
+     * @return the latest result of condition() before returning to the caller.
+     */
     template <typename Condition, typename Duration>
     auto wait_for(Condition && condition, Duration duration)
     {
@@ -53,6 +63,24 @@ public:
         return m_condition.wait(lock, duration, [&]() { return condition(m_data); });
     }
 
+    /**
+     * @brief executes @p action after waiting for @p condition
+     */
+    template <typename Condition, typename Action>
+    void with_lock_and_notify(Condition && condition, Action && action)
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_condition.wait(lock, [&]() { return condition(m_data); });
+
+        action(m_data);
+        lock.unlock();
+        m_condition.notify_one();
+    }
+
+    /**
+     * @brief executes @p action after waiting for @p condition, where @p action returns a result
+     * @return the result of action()
+     */
     template <typename Condition, typename Action>
     auto with_lock_and_notify_r(Condition && condition, Action && action)
     {
@@ -63,17 +91,6 @@ public:
         lock.unlock();
         m_condition.notify_one();
         return result;
-    }
-
-    template <typename Condition, typename Action>
-    void with_lock_and_notify(Condition && condition, Action && action)
-    {
-        std::unique_lock<std::mutex> lock(m_mutex);
-        m_condition.wait(lock, [&]() { return condition(m_data); });
-
-        action(m_data);
-        lock.unlock();
-        m_condition.notify_one();
     }
 };
 
